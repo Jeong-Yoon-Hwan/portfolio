87c1234167ece98626a853272179ec956c2a7daf
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2022-12-13 17:44:23 +0900

    스토어 생성

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import "./style.css"
 import Main from './pages/main'
 import Nav from './pages/nav'
 
+import { countStore } from "./store/store"
+
+
 const root = document.getElementById('root')
 
 
 root.appendChild(Nav) 
 root.appendChild(Main)
+
+
+
---
+++ src/store/store.js
@@ -0,0 +1,15 @@
+import { createStore } from "redux";
+
+const countModifier = (count = 0, action) => {
+  if(action.type === "ADD"){
+    return count + 1;
+  }else if(action.type === "DEL"){
+    return count - 1;
+  }
+  return count;
+}
+
+export const countStore = createStore(countModifier)
+
+
+console.log(countStore.getState())
