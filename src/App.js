3e7194b82ee094ed7894f11b4a5f3e07acc267ca
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2022-12-18 22:55:52 +0900

    nav 컴포넌트 생성 countStore 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import Main from "./component/Main";
+import Nav from "./component/Nav";
 
 const App = () =>{
   return(
-    <div>
-      test
-    </div>
+    <Container>
+      <Nav/>
+      <Main/>
+    </Container>
   )
   
 }
 export default App;
+
+const Container = styled.div`
+  width:100vw;
+  height:100vh;
+`
