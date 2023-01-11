a1767e020e8434cce5e10b6fa04b8ee75757c2db
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2023-01-12 01:33:49 +0900

    제목박스 생성

---
+++ src/component/Title.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const Title = () =>{
+  return(
+    <Container>
+      Title
+    </Container>
+  )
+}
+
+export default Title;
+
+const Container = styled.div`
+  width:100%;
+  height:30%;
+  background-color: rgba(182,87,84);
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  color:white;
+  font-size: 1.2rem;
+  font-weight:bold;
+`
