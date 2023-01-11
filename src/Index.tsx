4e39c92fff3e07be14fd1d9b92b80949e328e73d
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2023-01-12 01:32:47 +0900

    버전 버그 수정

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,4 +1,6 @@
-import ReactDOM from "react-dom"
-import App from "./App";
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+import App from "./App";
+import { createRoot } from "react-dom/client"
+const container:any = document.getElementById('root');
+const root = createRoot(container)
+root.render(<App/>)
